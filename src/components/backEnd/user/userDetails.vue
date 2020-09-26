960e8a561f660b195268d39114c97b19a68df55e
naser iziy <iziy_naser@yahoo.com>
2020-09-26 21:34:42 +0330

    add uer menu label to bundle
    add user's menu label to bundles

---
+++ src/components/backEnd/user/userDetails.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+           <f2fTable :items="items"  :fields="fields" caption="mm"  />                  
+    </div>
+</template>
+
+<script>
+
+import * as axios from 'axios';
+import f2fTable from '../../base/f2fTable'
+
+export default {
+    name:'userDetails',
+    data(){
+      return {
+        isBusy:false,
+        errors:{},  
+        items:[],    
+        fields:[
+            {key:"id",label:'title'},
+            {key:"username",label:'user name'}
+        ],            
+      } 
+    },
+    methods:{
+        listOfCategoriesRelatedToProduct(){
+                var self = this;
+                this.errors= {};
+                this.isBusy = true;
+                axios({
+                    method:'GET',
+                    url:'http://localhost:8080/f2f/management/user/list',
+                })
+                .then(function(res){
+                      console.log(res.data);  
+                      self.items = res.data.data; 
+                })
+                .catch(function(error){
+                    console.log(error)    ;
+                });
+        },
+    },
+    components:{
+        f2fTable
+    } ,
+    mounted(){
+            this.listOfCategoriesRelatedToProduct();
+    }      
+}
+</script>
+
+<style scoped>
+
+</style>
