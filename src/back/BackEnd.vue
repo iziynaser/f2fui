47f99a4c999a05b47885fbfe94d9da251817e791
iziy <iziy_naser@yahoo.com>
2023-07-17 12:26:57 +0330

    Create BackEnd.vue
    create backEnd component
    type : improvement
    purpose : security reason
    purpose2: seperate back/front vue pages

---
+++ src/back/BackEnd.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <NavBars/>
+        <b-card
+            title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+        >
+            <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+           </b-card-text>
+           <!-- <b-button href="#" variant="primary">Go somewhere</b-button> -->
+        </b-card>
+
+    </div>
+  </template>
+  
+  <script>
+import NavBars from '../components/navBars.vue';
+
+    export default {
+    name: "BackEnd",
+    components: { NavBars }
+}
+  </script>
+  
+  <style>
+  
+  </style>
+  
