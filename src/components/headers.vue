8f154b247046d8b6499472a27c633ee5a238ec30
iziy <iziy_naser@yahoo.com>
2023-07-17 13:43:53 +0330

    Update headers.vue

--- src/components/headers.vue
+++ src/components/headers.vue
@@ -1,12 +1,12 @@
 <template>
     <div >
-        <div class="container">
+        <!-- <div class="container">
             <div class="row align-items-start">
                 <div class="col-md-auto">
                     <b-button type="button" class="btn bg-light btn-outline-dark">       
                         <router-link to="/signUp" >{{$t('signUp')}}</router-link>
-                    </b-button>    
-                </div>    
+                    </b-button> 
+                </div>  
                 <div class="col col-lg-2">
                     <shoppingList/>   
                 </div>    
@@ -18,21 +18,22 @@
             </div>
         </div>
         <div class="pt-2">
-            <navBars/>        
-        </div>            
+             <navBars/>     
+        </div>        -->
     </div>
 </template>
 
 <script>
-import navBars from '../components/navBars'
-import shoppingList from '../components/shoppingList'
-import shoppingCart from '../components/shoppingCart'
+//import navBars from '../components/navBars'
+// import shoppingList from '../components/shoppingList'
+// import shoppingCart from '../components/shoppingCart'
 
 export default {
     name:'headers',
     components:{
-        navBars,shoppingList,shoppingCart
-    }    
+        // navBars,
+        // shoppingList,shoppingCart
+    } 
 }
 </script>
 
