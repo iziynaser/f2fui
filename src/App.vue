8eba276a2c6c056ad68f27c25346578169ba31a1
iziy <iziy_naser@yahoo.com>
2023-07-17 13:40:11 +0330

    Update App.vue
    add header site component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,28 @@
 <template>
   <div id="app" class="app-wrapper">
-    
-    <headers/>   
-    <main class="container" role="main">
-      
-      <toast :show="toast.show" :text="toast.text" @hide-toast="hideToast"></toast>
-      <router-view></router-view> 
-      
-    </main>   
-    <footers/> 
-
-
+      <HeaderSite/>   
+        <main class="container" role="main">
+          <toast :show="toast.show" :text="toast.text" @hide-toast="hideToast"></toast>
+          <router-view></router-view>      
+      </main>  
+      <footers/> 
   </div>
 </template>
 
 <script>
 
-import headers from './components/headers'
 import footers from './components/footers'
 
+import HeaderSite from './front/HeaderSite.vue'
 import toast from './components/Toast'
 
 export default {
   name: 'App',
   components: {
-        headers,
-        footers,
-        toast,
-  },
+    HeaderSite,
+    footers,
+    toast,
+},
   computed:{
         toast(){
           return this.$store.getters.toast;
