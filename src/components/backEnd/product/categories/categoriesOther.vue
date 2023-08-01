f8110f481177e85e1d7b4459f7c533b714e87cbb
iziy <iziy_naser@yahoo.com>
2023-08-01 17:10:24 +0330

    updating categories.vue
    add data part to component

--- src/components/backEnd/product/categories/categoriesOther.vue
+++ src/components/backEnd/product/categories/categoriesOther.vue
@@ -2,17 +2,17 @@
     <div>          
         <ul class="list-unstyled">
                 <li>
-                    <b-form-checkbox v-model="checked" name="checkButton" switch>
+                    <b-form-checkbox v-model="promotionProduct" name="promotionProductCheckButton" switch>
                         {{$t('categories_promotion')}}
                     </b-form-checkbox>
                   </li>
                 <li>
-                    <b-form-checkbox v-model="checked" name="checkButton" switch>
+                    <b-form-checkbox v-model="availableProduct" name="availableProductCheckButton" switch>
                         {{$t('categoriesKalaye_mojood')}}
                     </b-form-checkbox>                  
                 </li>
                 <li>
-                    <b-form-checkbox v-model="checked" name="checkButton" switch>
+                    <b-form-checkbox v-model="originalProduct" name="originalProductCheckButton" switch>
                         {{$t('categories_kalayeAsl')}}
                     </b-form-checkbox>                                    
                 </li>
@@ -28,7 +28,9 @@ export default {
           } ,
     data(){
       return {
-        
+        originalProduct : '' ,
+        availableProduct : '',
+        promotionProduct : ''
       }
     } ,
     methods:{
