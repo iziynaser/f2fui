3974b3218cfc3b58573a781e05cf14fc034f903f
iziy <iziy@behsazan.net>
2024-08-25 12:37:15 +0330

    add qa locale
    add qa locale

--- src/components/content/qa/qaMain.vue
+++ src/components/content/qa/qaMain.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
         qa
-              <b-card no-body>
-                  <b-tabs pills card>
-                        <b-tab title="create public qa">
-                            <qaContent/>
-                        </b-tab>
-                        <b-tab title="search">
-                            <qaSearch/>
-                        </b-tab>
-                        <b-tab title="public qa">
-                                <publicQa/>
-                                <br/>
-                        </b-tab>
-                  </b-tabs>
-              </b-card>
+        <b-card no-body>
+            <b-tabs pills card>
+                <b-tab :title="this.$t('qa_mtab_create')">
+                    <qaContent />
+                </b-tab>
+                <b-tab :title="this.$t('qa_mtab_search')">
+                    <qaSearch />
+                </b-tab>
+                <b-tab :title="this.$t('qa_mtab_paq')">
+                    <publicQa />
+                    <br />
+                </b-tab>
+            </b-tabs>
+        </b-card>
     </div>
 </template>
 
@@ -25,25 +25,23 @@ import qaSearch from './qaSearch'
 import publicQa from './publicQa'
 
 export default {
-    name:'qaMain',
-    props:{
-          } ,
-    data(){
-      return {
-        
-      }
-    } ,
-    methods:{
+    name: 'qaMain',
+    props: {
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
 
     },
-    components:{
+    components: {
         qaContent,
-        qaSearch ,
+        qaSearch,
         publicQa
     }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
