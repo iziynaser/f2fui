00c8a77dfdb30d41dd209276b782d3adac4599ab
iziy <iziy@behsazan.net>
2024-08-27 15:26:25 +0330

    add locale to proj
    add locale to proj

--- src/components/backEnd/product/comment/commentDigram.vue
+++ src/components/backEnd/product/comment/commentDigram.vue
@@ -1,42 +1,38 @@
 <template>
+  <div>
+    {{ $t('overview _comments _product') }}
     <div>
-                            overview of comments for this product
-                  <div>
-                    <!-- <label></label> -->
-                    <div class="mt-2">label one</div>
-                    <b-progress value="25" striped variant="success"></b-progress>
 
-                    <!-- <label></label> -->
-                    <div class="mt-2">label2 for ppppp</div>
-                    <b-progress  value="75" striped variant="info"></b-progress>
+      <div class="mt-2">{{ $t('comment_1') }}</div>
+      <b-progress value="25" striped variant="success"></b-progress>
 
-                    <!-- <label></label> -->
-                    <div class="mt-2">label3 for product</div>
-                    <b-progress value="35" striped variant="warning"></b-progress>
+      <!-- <label></label> -->
+      <div class="mt-2">{{ $t('comment_2') }}</div>
+      <b-progress value="75" striped variant="info"></b-progress>
 
-                    <!-- <label></label> -->
-                    <div class="mt-2">label four fr</div>
-                    <b-progress value="90" striped variant="danger"></b-progress>
-                  </div>
+      <div class="mt-2">{{ $t('comment_3') }}</div>
+      <b-progress value="35" striped variant="warning"></b-progress>
+
+      <div class="mt-2">{{ $t('comment_4') }}</div>
+      <b-progress value="90" striped variant="danger"></b-progress>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'commentDiagram',
-    props:{
-          } ,
-    data(){
-      return {
-        
-      }
-    } ,
-    methods:{
+  name: 'commentDiagram',
+  props: {
+  },
+  data() {
+    return {
 
-    }      
+    }
+  },
+  methods: {
+
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
