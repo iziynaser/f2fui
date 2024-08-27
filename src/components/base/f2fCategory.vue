5859e12b60275835c9207378cbc4e10d4c42c480
iziy <iziy@behsazan.net>
2024-08-27 14:30:19 +0330

    add locales to proj
    add locales to proj

--- src/components/base/f2fCategory.vue
+++ src/components/base/f2fCategory.vue
@@ -1,18 +1,17 @@
 <template>
     <div>
         <div class="p-2 my-2 border">
-            categories
-            <hr/>
+            {{ $t('categories') }}
+            <hr />
             <b-row class="my-1">
-                <b-col >
+                <b-col>
                     <b-card>
-                    <b-form-checkbox-group stacked name="checkButton">      
-                        <b-form-checkbox :value="option.id" v-for="option in cols1" 
-                                         :key="option.id" 
-                                         @change="loadCategories(option.id,option.name)">
-                            {{option.name}}
-                        </b-form-checkbox>        
-                    </b-form-checkbox-group>
+                        <b-form-checkbox-group stacked name="checkButton">
+                            <b-form-checkbox :value="option.id" v-for="option in cols1" :key="option.id"
+                                @change="loadCategories(option.id, option.name)">
+                                {{ option.name }}
+                            </b-form-checkbox>
+                        </b-form-checkbox-group>
                     </b-card>
                 </b-col>
             </b-row>
@@ -26,101 +25,99 @@
 import * as axios from 'axios'
 
 export default {
-    name:'f2fCategory',
-    props:{
-          } ,
-    data(){
-      return {
-        value:[],
-        cols1:[],
-        breadcrumbItems:[],
-        brands:[],
-        cCode:"",
-        cName:"",
-        form:{
-            name:"",
-            value:"",
-            categoryId:""
+    name: 'f2fCategory',
+    props: {
+    },
+    data() {
+        return {
+            value: [],
+            cols1: [],
+            breadcrumbItems: [],
+            brands: [],
+            cCode: "",
+            cName: "",
+            form: {
+                name: "",
+                value: "",
+                categoryId: ""
+            }
         }
-      }
-    } ,
-    methods:{
-        variantColors(idx){
-            return idx % 2==0 ? "light" : "info" ;
+    },
+    methods: {
+        variantColors(idx) {
+            return idx % 2 == 0 ? "light" : "info";
         },
-        loadCategories(idv,idt){
-                console.log('idv:'+idv+',idt:'+idt);
-                var self = this;
+        loadCategories(idv, idt) {
+            console.log('idv:' + idv + ',idt:' + idt);
+            var self = this;
 
-                if(idv==1 && self.breadcrumbItems.length>1){
-                    self.breadcrumbItems = self.breadcrumbItems.splice(0);
-                }
-                
-                self.form.categoryId=idv;
-                self.cName = idt;
+            if (idv == 1 && self.breadcrumbItems.length > 1) {
+                self.breadcrumbItems = self.breadcrumbItems.splice(0);
+            }
 
-                const url = `/Category/listById`;
-                axios.get(url,{
-                params:{id:idv}
+            self.form.categoryId = idv;
+            self.cName = idt;
+
+            const url = `/Category/listById`;
+            axios.get(url, {
+                params: { id: idv }
             })
-                .then(function(res){
-                    if(res){
+                .then(function (res) {
+                    if (res) {
                         self.cols1 = res.data;
-                        if(idv!==1){
-                            let exist=false;
-                            let idx=-1;
-                            for(const [i,b] of self.breadcrumbItems.entries()){
-                                if(b.id==idv){
-                                     exist = true;
-                                     idx=i;
-                                }                                    
+                        if (idv !== 1) {
+                            let exist = false;
+                            let idx = -1;
+                            for (const [i, b] of self.breadcrumbItems.entries()) {
+                                if (b.id == idv) {
+                                    exist = true;
+                                    idx = i;
+                                }
                             }
-                            if(exist==false)        
-                                self.breadcrumbItems.push({text:idt,id:idv,href:'#'});
-                            else{
-                                let L = self.breadcrumbItems.length-1 ;
-                                if(L!==idx)
+                            if (exist == false)
+                                self.breadcrumbItems.push({ text: idt, id: idv, href: '#' });
+                            else {
+                                let L = self.breadcrumbItems.length - 1;
+                                if (L !== idx)
                                     var k = 0;
-                                    while(k<L-idx){
-                                         self.breadcrumbItems.pop();
-                                         k++;   
-                                    }                                        
+                                while (k < L - idx) {
+                                    self.breadcrumbItems.pop();
+                                    k++;
+                                }
                             }
-                                
-                        }else{
-                            self.breadcrumbItems=[];
+
+                        } else {
+                            self.breadcrumbItems = [];
                         }
 
                         //load brands
                         self.loadBrands(idv);
-                    }                        
+                    }
                 })
-                .catch(function(error){
-                    console.log(error)    ;
+                .catch(function (error) {
+                    console.log(error);
                 });
         },
-        loadBrands(cId){
+        loadBrands(cId) {
             var self = this;
             const url = `/brand/byCategoryId`;
-            axios.get(url,{
-                params:{
+            axios.get(url, {
+                params: {
                     categoryId: cId
                 }
             })
-                .then(function(res){
+                .then(function (res) {
                     self.brands = res.data;
                 })
-                .catch(function(error){
-                    console.log(error)    ;
+                .catch(function (error) {
+                    console.log(error);
                 });
         }
     }
-    ,created(){
-        this.loadCategories(1,'همه دسته ها');
-    }      
+    , created() {
+        this.loadCategories(1, 'همه دسته ها');
+    }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/footers.vue
+++ src/components/footers.vue
@@ -1,96 +1,96 @@
 <template>
-<div>
-  <b-row>
-    <b-col></b-col>
-    <b-col></b-col>
-    <b-col></b-col>
-    <b-col>
-      <router-link to="/publicQa">answer to public questions</router-link>
-    </b-col>
-  </b-row>
-  <hr/>
-  <div>browse to top</div>
-  <hr/>
   <div>
     <b-row>
-      <b-col>
-          <b-list-group horizontal="md" flush>
-              <b-list-group-item>{{$t('C_FOOTER_REGISTER_ORDER')}}</b-list-group-item>
-              <b-list-group-item>{{$t('C_FOOTER_PROCESS_ORDER')}}</b-list-group-item>
-              <b-list-group-item>{{$t('C_FOOTER_PAYMENT_METHODS')}}</b-list-group-item>
-          </b-list-group>
-      </b-col>
-      <b-col>          
-          <b-list-group horizontal="md" flush>
-            <li>{{$t('C_FOOTER_REPORT_A_BUG')}}</li>
-            <li>{{$t('C_FOOTER_PRIVACY')}}</li>
-            <li>{{$t('C_FOOTER_RETURN_PRODUCT')}}</li>
-          </b-list-group>
-      </b-col>
+      <b-col></b-col>
+      <b-col></b-col>
       <b-col></b-col>
       <b-col>
+        <router-link to="/publicQa">{{ $t('answer_public_questions') }}</router-link>
+      </b-col>
+    </b-row>
+    <hr />
+    <div>{{ $t('browse_to_top') }}</div>
+    <hr />
+    <div>
+      <b-row>
+        <b-col>
+          <b-list-group horizontal="md" flush>
+            <b-list-group-item>{{ $t('C_FOOTER_REGISTER_ORDER') }}</b-list-group-item>
+            <b-list-group-item>{{ $t('C_FOOTER_PROCESS_ORDER') }}</b-list-group-item>
+            <b-list-group-item>{{ $t('C_FOOTER_PAYMENT_METHODS') }}</b-list-group-item>
+          </b-list-group>
+        </b-col>
+        <b-col>
+          <b-list-group horizontal="md" flush>
+            <li>{{ $t('C_FOOTER_REPORT_A_BUG') }}</li>
+            <li>{{ $t('C_FOOTER_PRIVACY') }}</li>
+            <li>{{ $t('C_FOOTER_RETURN_PRODUCT') }}</li>
+          </b-list-group>
+        </b-col>
+        <b-col></b-col>
+        <b-col>
           <b-row></b-row>
           <b-row>
             <b-col>
               <b-row>
                 inform from pormotions
-                <b-input type="text"/>
+                <b-input type="text" />
               </b-row>
             </b-col>
           </b-row>
-      </b-col>
-    </b-row>
-  </div>
-<hr/>
-  <div>
+        </b-col>
+      </b-row>
+    </div>
+    <hr />
+    <div>
       <b-row>
         <b-col>
-                  <span>{{$t('C_FOOTER_FOLLOW_US')}}</span>
-                  <b-link :href="form.instagramUrl">
-                      <b-img :src="urlI('/images/footer/instagram.png')" class="p-2 w-20 img-rounded" />                      
-                  </b-link>
-                  <b-link :href="form.twitterUrl">
-                      <b-img :src="urlI('/images/footer/twitter.png')" class="p-2 w-20 img-rounded" />
-                  </b-link>
-                  <b-link :href="form.aparatUrl">
-                      <b-img :src="urlI('/images/footer/aparat.png')" class="p-2 w-20 img-rounded" />
-                  </b-link>
-                  <b-link :href="form.linkedInUrl">
-                      <b-img :src="urlI('/images/footer/linkedin.png')" class="p-2 w-20 img-rounded" />
-                  </b-link>
+          <span>{{ $t('C_FOOTER_FOLLOW_US') }}</span>
+          <b-link :href="form.instagramUrl">
+            <b-img :src="urlI('/images/footer/instagram.png')" class="p-2 w-20 img-rounded" />
+          </b-link>
+          <b-link :href="form.twitterUrl">
+            <b-img :src="urlI('/images/footer/twitter.png')" class="p-2 w-20 img-rounded" />
+          </b-link>
+          <b-link :href="form.aparatUrl">
+            <b-img :src="urlI('/images/footer/aparat.png')" class="p-2 w-20 img-rounded" />
+          </b-link>
+          <b-link :href="form.linkedInUrl">
+            <b-img :src="urlI('/images/footer/linkedin.png')" class="p-2 w-20 img-rounded" />
+          </b-link>
         </b-col>
-     </b-row>
+      </b-row>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 
-import {baseUrl} from '../api/util'
+import { baseUrl } from '../api/util'
 
 export default {
-    name:'footers',   
-    data(){
-        return{
-        errorMessage:'',
-          form:{
-              linkedInUrl:'',
-              twitterUrl:'',
-              aparatUrl:'',
-              instagramUrl:''
-          }
-        }
-    },
-    methods:{
-      urlI(u){
-        return baseUrl(u)
+  name: 'footers',
+  data() {
+    return {
+      errorMessage: '',
+      form: {
+        linkedInUrl: '',
+        twitterUrl: '',
+        aparatUrl: '',
+        instagramUrl: ''
       }
-    } 
+    }
+  },
+  methods: {
+    urlI(u) {
+      return baseUrl(u)
+    }
+  }
 }
 </script>
 
 <style scoped>
- .navbar {
+.navbar {
   position: fixed;
   left: 0;
   bottom: 0;
@@ -100,5 +100,5 @@ export default {
   background-color: red;
   color: white;
   text-align: center;
- }
+}
 </style>
