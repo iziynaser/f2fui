bb663add80d35c6f387ced8aed4ec1ea377076ec
iziy <iziy@behsazan.net>
2024-10-01 10:40:10 +0330

    changes
    changes :
                   add additional description to package.json like author,description,...
                   add personal pages like note,calendar,appversion ,... to main vue page
                   add persian calendar to project and try to customie it

--- src/back/BackEnd.vue
+++ src/back/BackEnd.vue
@@ -10,22 +10,22 @@
                         <appInfo />
                     </b-tab>
                     <b-tab title="calendar">
-                        sdfsdfsdfsdf
+                        <calendar />
                     </b-tab>
                     <b-tab title="phone">
-                        sdfsdfsdfsdf
+                        <phone />
                     </b-tab>
                     <b-tab title="notes">
-                        sdfsdfsdfsdf
+                        <notes />
                     </b-tab>
                     <b-tab title="links">
-                        sdfsdfsdfsdf
+                        <links />
                     </b-tab>
                     <b-tab title="charts">
-                        sdfsdfsdfsdf
+                        <charts />
                     </b-tab>
                     <b-tab title="softwares">
-                        sdfsdfsdfsdf
+                        <softwares />
                     </b-tab>
                 </b-tabs>
             </b-card>
@@ -36,12 +36,24 @@
 <script>
 import NavBars from '../components/navBars.vue';
 import appInfo from './appInfo.vue';
+import charts from '../components/portal/charts.vue';
+import links from '../components/portal/links.vue';
+import calendar from '../components/portal/calendar.vue';
+import notes from '../components/portal/notes.vue';
+import phone from '../components/portal/phone.vue';
+import softwares from '../components/portal/softwares.vue';
 
 export default {
     name: "BackEnd",
     components: {
         NavBars,
-        appInfo
+        appInfo,
+        charts,
+        links,
+        calendar,
+        notes,
+        phone,
+        softwares
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {BootstrapVue,BootstrapVueIcons} from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 //import BootstrapVue from 'bootstrap-vue'
 //import BootstrapVueIcons from 'bootstrap-vue'
 //import {BootstrapVue,BootstrapVueIcons, ToastPlugin} from 'bootstrap-vue'
@@ -14,13 +14,13 @@ import VueI18Next from '@panter/vue-i18next';
 
 import i18next from 'i18next';
 
-import axios from 'axios' ;
+import axios from 'axios';
 
-import VueAxios from 'vue-axios' ;
+import VueAxios from 'vue-axios';
 
-import pdate from './components/pdate' ;
-import enJson from './locales/en' ;
-import faJson from './locales/fa' ;
+import pdate from './components/pdate';
+import enJson from './locales/en';
+import faJson from './locales/fa';
 import f2fInvoiceType from './components/base/f2fInvoiceType';
 //import toast from './components/Toast'
 
@@ -28,13 +28,19 @@ import f2fInvoiceType from './components/base/f2fInvoiceType';
 //import f2fApi from './f2fApi' ;
 
 //test appUrl method
-import {appUrl,baseUrl} from './api/util'
+import { appUrl, baseUrl } from './api/util'
 
 //test interceptor
 import interceptorSetup from './interceptors'
 
 //test default persian align and trl
 import { f2fChangeLocale } from './f2fUtils';
+
+//add new calendr for event management
+//import moment from 'vue-jalali-moment'
+//Vue.use(moment)
+//import PersianCalendar from 'vue-persian-calendar';
+//Vue.use(PersianCalendar)
 
 Vue.config.productionTip = false
 
@@ -47,57 +53,57 @@ Vue.use(VueI18Next);
 
 //new setting
 axios.interceptors.request.use(
-  function(config){
+  function (config) {
     config.headers.authorization = localStorage.getItem('access_token');
-    config.params = {...config.params, access_token: localStorage.access_token}
-    
-    if(config.url.includes("oauth") || config.url.includes("captcha"))
+    config.params = { ...config.params, access_token: localStorage.access_token }
+
+    if (config.url.includes("oauth") || config.url.includes("captcha"))
       config.url = baseUrl(config.url);
     else
       config.url = appUrl(config.url);
 
-      console.log(config.url);
-      return config;
+    console.log(config.url);
+    return config;
   },
 );
 
 axios.interceptors.response.use(
-  function(response){
+  function (response) {
     return response;
-  },function(error){
-    
-    if(error.message.includes('Network Error')) {
-      store.commit("showToast",i18next.t('TOKEN_EXPIRED_ERR'));
+  }, function (error) {
+
+    if (error.message.includes('Network Error')) {
+      store.commit("showToast", i18next.t('TOKEN_EXPIRED_ERR'));
       router.push('/login')
     }
-    else if(error.response.data){
-      if(error.response.data.status==400)
-          store.commit("showToast","Bad Request...400.");
-      else if(error.response.data.status==401)
-          store.commit("showToast","Bad Request...401.");
-      else if(error.response.data.status==404)
-          store.commit("showToast","Bad Request...404.");
-      
-      if(error.response.status==400 && error.response.data.error==="invalid_grant")
-            store.commit("showToast","username and password do not match");
+    else if (error.response.data) {
+      if (error.response.data.status == 400)
+        store.commit("showToast", "Bad Request...400.");
+      else if (error.response.data.status == 401)
+        store.commit("showToast", "Bad Request...401.");
+      else if (error.response.data.status == 404)
+        store.commit("showToast", "Bad Request...404.");
 
-          console.log("error:"+error.response.data.error);// = "Bad Request"
-          console.log("message:"+error.response.data.message);
-          console.log("path:"+error.response.data.path);
-          console.log("status:"+error.response.data.status);
-          console.log("trace:"+error.response.data.trace);				
-          
-    }        
+      if (error.response.status == 400 && error.response.data.error === "invalid_grant")
+        store.commit("showToast", "username and password do not match");
+
+      console.log("error:" + error.response.data.error);// = "Bad Request"
+      console.log("message:" + error.response.data.message);
+      console.log("path:" + error.response.data.path);
+      console.log("status:" + error.response.data.status);
+      console.log("trace:" + error.response.data.trace);
+
+    }
     //throw new Error("invalid token detected"+ error)
   }
-  );
+);
 //end of new setting
 
-Vue.use(VueAxios,axios)
+Vue.use(VueAxios, axios)
 
 //use customize components
-Vue.component('pdate',pdate);
-Vue.component('f2fInvoiceType',f2fInvoiceType);
+Vue.component('pdate', pdate);
+Vue.component('f2fInvoiceType', f2fInvoiceType);
 
 //use toast for showing messages
 //Vue.component('toast',toast)
@@ -106,18 +112,18 @@ Vue.component('f2fInvoiceType',f2fInvoiceType);
 interceptorSetup()
 
 i18next.init({
-   lng:'fa',
-   fallbackLng:'fa',
-   whitelist:['en','fa'],
-   debug:true,
-   resources:{
-     en : enJson,
-     fa:faJson
-   }
-},(err,t)=>{
-    if(err)
-      return console.log('something went wrong loading',err);
-    t('key');
+  lng: 'fa',
+  fallbackLng: 'fa',
+  whitelist: ['en', 'fa'],
+  debug: true,
+  resources: {
+    en: enJson,
+    fa: faJson
+  }
+}, (err, t) => {
+  if (err)
+    return console.log('something went wrong loading', err);
+  t('key');
 });
 const i18n = new VueI18Next(i18next);
 
@@ -129,7 +135,7 @@ f2fChangeLocale('fa');
 new Vue({
   router,
   store,
-  i18n:i18n,
+  i18n: i18n,
   BootstrapVue,
   BootstrapVueIcons,
   //axios:axios,
